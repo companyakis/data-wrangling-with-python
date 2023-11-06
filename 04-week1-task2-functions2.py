9b66938508c56b71b2cf21963d5210f9c4a972a4
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2023-11-06 21:30:02 +0300

    Create 04-week1-task2-functions2.py

---
+++ 04-week1-task2-functions2.py
@@ -0,0 +1,17 @@
+"""
+Task: Define a function sayhi(n) that:
+1. n is by default 1, if user doesn't pass an argument to the function.
+2. print 'Welcome to Python Functions' n times.
+
+Task: Call sayhi(n) to print out 0, 1, and 5 messages
+
+"""
+
+def sayhi(n = 1):
+  for i in range(n):
+    print("Welcome to Python Functions")
+  
+
+#sayhi(0)
+#sayhi(1)
+#sayhi(5)
